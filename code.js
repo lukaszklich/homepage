c5e1364bd764b412d3e2d6e545a89abf79ae7b45
Lukasz Klich <lukaszklich@gmail.com>
2019-06-23 15:55:46 +0100

    feat: JS test

--- code.js
+++ code.js
@@ -1,3 +1,6 @@
 const name = 'Łukasz';
 const age = 34;
-console.log(`Cześć, mam na imię ${name} i mam ${age} lat.`);
+console.log(`Cześć, mam na imię ${name} i mam ${age} lat. ☕️`);
+const about = document.querySelector('.about__paragraph--js');
+console.log(about.innerHTML);
+about.innerHTML = `Cześć, mam na imię ${name} i mam ${age} lata ☕️`;
